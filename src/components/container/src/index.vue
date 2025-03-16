1f1bd9a58b27bf4b5caedd2850bff7147f336b0e
Licx <skylicx@163.com>
2025-03-17 00:58:27 +0800

    伸缩菜单抽离头部和侧边栏

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,27 +1,11 @@
 <template>
   <el-container>
     <el-aside width="auto">
-      <el-menu default-active="1" class="el-menu-vertical-demo" :collapse="isCollapse">
-        <el-menu-item index="1">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航2</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航3</span>
-        </el-menu-item>
-      </el-menu>
+      <nav-side :collapse="isCollapse" />
     </el-aside>
     <el-container>
       <el-header>
-        <span @click="toggle">
-          <el-icon v-if="isCollapse"><el-icon-expand /></el-icon>
-          <el-icon v-else><el-icon-fold /></el-icon>
-        </span>
+        <nav-header v-model:collapse="isCollapse" />
       </el-header>
       <el-main>
         <router-view></router-view>
@@ -31,18 +15,16 @@
 </template>
 
 <script setup lang="ts">
+import NavSide from './navSide/index.vue'
+import NavHeader from './navHeader/index.vue'
 import { ref } from 'vue'
 
 const isCollapse = ref(false)
-
-const toggle = () => {
-  isCollapse.value = !isCollapse.value
-}
 </script>
 
 <style lang="scss" scoped>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+.el-header {
+  padding: 0;
+  border-bottom: 1px solid #eee;
 }
 </style>
