0d062235bbbc26c4e27448051d8396080f67826a
Licx <skylicx@163.com>
2025-03-18 15:25:51 +0800

    单独打包按需导入

--- command/build.js
+++ command/build.js
@@ -2,6 +2,8 @@ import path from 'path'
 import { defineConfig, build } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'url'
+import fsExtra from 'fs-extra'
+import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -44,9 +46,54 @@ const buildAll = async () => {
   })
 }
 
+// 单组件打包
+const buildSingle = async (name) => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, name),
+        name: 'index',
+        fileName: 'index',
+        formats: ['es', 'umd'],
+      },
+      outDir: path.resolve(outDir, name),
+    }
+  })
+}
+
+// 每个组件生成package.json
+const createPackageJson = (name) => {
+  const fileStr = `{
+    "name": "${name}",
+    "main": "index.js",
+    "module": "index.esm.js",
+    "style": "style.css"
+  }`
+  // 输出
+  fsExtra.outputFile(
+    path.resolve(outDir, `${name}/package.json`),
+    fileStr,
+    'utf-8'
+  )
+}
+
 // 打包成库
 const buildLib = async () => {
   await buildAll()
+
+  // 获取组件名称数组
+  const components = fs.readdirSync(entryDir).filter(name => {
+    const componentDir = path.resolve(entryDir, name)
+    const isDir = fs.lstatSync(componentDir).isDirectory()
+    return isDir && fs.readdirSync(componentDir).includes('index.ts')
+  })
+
+  for(const name of components) {
+    await buildSingle(name)
+    createPackageJson(name)
+  }
 }
 
 buildLib()
--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,17 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { toLine } from './utils'
-// import mUI from './components'
-import mUI from '../lib/mooc-element-components'
-import '../lib/mooc-element-components.css'
+import mUI from './components'
+// import mUI from '../lib/mooc-element-components'
+// import '../lib/mooc-element-components.css'
+// 单独引入组件
+// import chooseIcon from '../lib/chooseIcon/index.js'
+// import '../lib/chooseIcon/index.css'
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
 app.use(mUI)
+// app.use(chooseIcon)
 app.mount('#app')
 
 // 全局注册图标 牺牲一点性能
