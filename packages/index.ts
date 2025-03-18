1a5679f8b7ee9ef1989a1e41ebf3b5335ead6964
Licx <skylicx@163.com>
2025-03-18 17:00:50 +0800

    打包更行

--- packages/index.ts
+++ packages/index.ts
@@ -2,16 +2,18 @@ import type { App } from 'vue'
 import chooseIcon from './chooseIcon'
 import './styles/base.scss'
 import './styles/ui.scss'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { toLine } from './utils'
 
-
-const components = [
-  chooseIcon
-]
+const components = [chooseIcon]
 
 export default {
   install(app: App) {
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(`el-icon-${toLine(key)}`, component)
+    }
     components.map(item => {
       app.use(item)
     })
-  }
+  },
 }
